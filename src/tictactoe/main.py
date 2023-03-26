6fbcc63deed207bcd0ad4a4c0110361117ba7188
alekulyn <alekulyn@users.noreply.github.com>
2023-03-25 19:06:43 -0500

    Update main.py
    Winning move checker

--- src/tictactoe/main.py
+++ src/tictactoe/main.py
@@ -34,9 +34,12 @@ def minimax(depth, nodeIndex, maximizingPlayer,
             if beta <= alpha:
                 break
         return best
+		
+class State:
+	board = [[0,0,0],[0,0,0],[0,0,0]]
+	turn = "X"
 
 class TicTacToe:
-	board = [[0,0,0],[0,0,0],[0,0,0]]
 	empty = [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3], [3,1], [3,2], [3,3]]
 	mode = "dumbass" #"bigbro" or "dumbass"
 	piece = 0	#X goes first
@@ -70,3 +73,27 @@ class TicTacToe:
 			random_move()
 		else mode == "bigbro":
 			minimax(depth = 3, maximizingPlayer = True)
+	
+	def check(direction, slot = -1):
+		win = true
+		if (directon == "row"):
+			for i in range(0,3):
+				win = (board[slot][i] != 0)
+		elif (direction == "col"):
+			for i in range(0,3):
+				win = (board[i][slot] != 0)
+		elif (direction == "diag"):
+			for i in range(0,3):
+				win = (board[i][i] != 0)
+		elif (direction == "antidiag"):
+			for i in range(0,3):
+				win = (board[2-i][i] != 0)
+		return win
+	
+	def iswinningmove(row, col):
+		win = check("row", row) || check("col", col)
+		if (row == col) || (row + col == 2):
+			win = win || check("diag") || check("antidiag")
+		return win
+			
+	#corner, middle, or center

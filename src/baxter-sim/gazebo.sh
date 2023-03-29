cb1fe29511b18e87a45d3253b7256ccba974d249
Nick Padilla <nicholas.padilla@npcomplete.io>
2023-03-29 18:21:12 -0500

    Add baxter sim files

---
+++ src/baxter-sim/gazebo.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+echo "Building Docker image"
+git_commit_hash=$(git rev-parse --short HEAD)
+docker build . -t csci-5551/baxter:$git_commit_hash
+
+echo "Starting Gazbeo"
+rocker --nvidia --x11 csci-5551/baxter:$git_commit_hash '/root/ros_ws/baxter.sh sim'

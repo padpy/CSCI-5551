4d97ce896068cc8c4dbcdb4a675fe639c226c412
Jonas <jonas060@d.umn.edu>
2023-04-26 13:45:39 -0700

    Node.py update

--- src/project_ws/src/baxter_moveit_controller/scripts/node.py
+++ src/project_ws/src/baxter_moveit_controller/scripts/node.py
@@ -4,6 +4,7 @@ import moveit_commander
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 import numpy as np
 from math import pi, tau, dist, fabs, cos
+import copy
 
 class BaxterMoveitController(object):
     def __init__(self):
@@ -53,55 +54,78 @@ class BaxterMoveitController(object):
         #Clear targets after planning poses.
         self.group.clear_pose_targets()
         
-    def move_to_base_position(self, base_coordinate):
+    def smooth_move_to_initial(self):
+        waypoints = []
+        waypoints_e= []
         
-        #orientation = quaternion_from_euler
-        #orientation = quaternion_from_euler(np.pi, 0, 0)
+        scale = 1.0
         
-        #orientation = quaternion_from_euler(roll, pitch, yaw)
-        #target_pose.orientation.x = orientation[0]
-        #target_pose.orientation.y = orientation[1]
-        #target_pose.orientation.z = orientation[2]
-        #target_pose.orientation.w = orientation[3]
+        wpose = self.group.get_current_pose(end_effector_link = 'left_gripper').pose
+        wpose.position.y +=  0.5  # First move up (z)
+        wpose.position.z +=  1.0
+        waypoints.append(copy.deepcopy(wpose))
         
-        target_pose = base_coordinate
-    
-        self.group.set_joint_value_target(base_coordinate)
+        (plan, fraction) = self.group.compute_cartesian_path(
+        waypoints, 0.01, 0.0)
+        print("Executing plan")
+        self.group.execute(plan, wait=True)
         
-        self.group.plan()
-
-        success = self.group.go(wait=True)
-        
-        #Ensures no residual movement
         self.group.stop()
         
         #Clear targets after planning poses.
-        self.group.clear_pose_targets()   
+        self.group.clear_pose_targets() 
+        
+        
+        group_variable_values = self.group.get_current_joint_values()
+        print("variable 1: " + str(group_variable_values[0]))
+        group_variable_values[0] = group_variable_values[0] - 1.4
+        self.group.set_joint_value_target(group_variable_values)
 
-        print("Returned to starting position: " + str(success))
+        plan2 = self.group.plan()
+        
+        self.group.go(wait = True)
+        
+        self.group.stop()
+        
+        print("Done!")
+        
+    def move_to_block_1(self):
+        continue
+        
 
     def _home(self):
         #current_pose = self.group.get_current_pose(end_effector_link='left_gripper').pose
         
-        #Move to initial State
-        self.move(0.0, 0.75, 0.25, np.pi, 0, 0)
-        self.move(0.75, 0.0, 0.25, np.pi, 0, 0)
-        
-        #Grab joint values for initial state
+        #Get Base Joint Values
         current_robot_pose = self.group.get_current_joint_values()
         
-        my_input = 0.0
+        #Move to initial State
+        self.smooth_move_to_initial()
         
-        while(my_input is not None):
-            my_input = [float(x) for x in input("Enter 6 value separated by a comma. First 3 values are position (x, y, z), last 3 are orientation (x, y, z).\n").split(', ')]
-            if(my_input is None):
-                break
-            else:
-                self.move(my_input[0], my_input[1], my_input[2], my_input[3], my_input[4], my_input[5])
-            #self.move(1.5, 0.0, -0.5, np.pi, 0, 0)
-                print("Moving back to initial position")
-                self.move_to_base_position(current_robot_pose)
-
+        #Move back to base position.
+        self.move_to_base_position(current_robot_pose)
+        
+        # my_input = "input"
+        # while(my_input is not None):
+            # my_input = [float(x) for x in input("Enter 6 value separated by a comma. First 3 values are position (x, y, z), last 3 are orientation (x, y, z).\n").split(', ')]
+            # if(my_input is None):
+                # break
+            # else:
+                # #print(self.shoulder.get_current_pose().pose)
+                
+                # self.move_joint(my_input[0], my_input[1])
+            # #self.move(1.5, 0.0, -0.5, np.pi, 0, 0)
+                # print("Moving back to initial position")
+                # self.group = moveit_commander.MoveGroupCommander("left_arm")
+                # self.move_to_base_position(current_robot_pose)
+                # group_names = self.robot.get_group_names()
+                
+                # #print(self.group.get_current_joint_values("left_s0"))
+                # print("============ Available Planning Groups:", self.robot.get_group_names())
+                # print("============ Available Planning Joints:", self.group.get_joints())
+                # self.group.stop()
+                # self.group.clear_pose_targets() 
+                
 
 if __name__ == '__main__':
     try:

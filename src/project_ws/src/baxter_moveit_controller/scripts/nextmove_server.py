1ed53ef89ee39015e3ce5c1eddc2f830c67bd9fa
drewosmundson <drewosmundson@gmail.com>
2023-05-09 22:19:32 -0500

    Implimented nextmove_server functions

--- src/project_ws/src/baxter_moveit_controller/scripts/nextmove_server.py
+++ src/project_ws/src/baxter_moveit_controller/scripts/nextmove_server.py
@@ -1,12 +1,36 @@
 #!/usr/bin/env python3
 import rospy
-import NextMove
+from NextMove import getNextMove
+# from std_msg.msg import SOMEARRAYCLASS
+from std_msgs.msg import (
+    UInt16,
+    UInt16MultiArray
+)
 
-def nextmove(req):
-    pass
+class MoveServer:
+    def __init__(self) -> None:
+        rospy.init_node("next_move_server")
+        self.pub = rospy.Publisher('/tictactoe/move', UInt16)
+        rospy.Subsciber('/tictactoe/board', UInt16, self.board_state_callback)
 
-def nextmove_server():
-    pass
+    def next_move(self, board):
+        mov_val = getNextMove(board)
+        return mov_val
+
+    def board_state_callback(self, msg):
+        board = msg.data 
+
+        if len(board) != 9:
+            return
+        move_msg = UInt16()
+        move_msg.data = getNextMove([board[0:3], board[3:6], board[6:9]])
+        self.pub(move_msg)
+        rospy.loginfo(f'Told Baxter to play {move_msg.data}')
+
+    def spin(self):
+        rospy.spin()
+
 
 if __name__ == "__main__":
-    nextmove_server()
+    move_server = MoveServer()
+    move_server.spin() 
